862b054937f991e486963aea8f39936189fb8d56
zakon.one <principdereva@gmail.com>
2022-07-04 15:34:27 +0400

    модальное окно

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// const btn = document.querySelectorAll(".open-modal-btn"); //Для одной кнопки
+const btns = document.querySelectorAll(".open-modal-btn"); //Много кнопок
+const form = document.querySelector(".search-modal");
+const closeBtn = document.querySelector(".modal-close-button");
+
+if (form) {
+  // btn.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   form.classList.add("is-active");
+  // });
+
+  btns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      form.classList.add("is-active");
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    form.classList.remove("is-active");
+  });
+}
